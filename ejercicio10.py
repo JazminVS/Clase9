acf02dd54fe820b7cc335940c28bde993ab561fa
JazminVS <jazminv8.95@gmail.com>
2016-06-16 09:44:58 -0500

    Ejercicio 10
    Creando un poligono y se movera en cada iteracion del for

---
+++ ejercicio10.py
@@ -0,0 +1,15 @@
+import time
+from tkinter import *
+tk=Tk()
+
+canvas=Canvas(tk, width=500, height=500)
+canvas.pack()
+canvas.create_polygon(10,10,10,60,50,35)
+
+for x in range (0,90):
+	canvas.move(1,5,0)
+	tk.update()
+	time.sleep(0.05)
+
+tk.mainloop()
+
